11fb5530d00e3697ed34882cb77c15507243b16a
Nicolas Offermans <nicolaso@cetuslac1.mcp.alcf.anl.gov>
2018-08-29 19:21:23 +0000

    Safer upper bound for max number of transferred local matrix entries

--- core/experimental/crs_hypre.f
+++ core/experimental/crs_hypre.f
@@ -161,7 +161,7 @@ c     GS data
       
 c     Crystal router data
       integer crh,nmax
-      parameter (nmax=1.2*lcr*(lelv+20)) ! somewhat arbitrary value
+      parameter (nmax=4*lcr*(lelv+100)) ! somewhat arbitrary value
       integer vi(lcr+2,nmax)
       integer*8 vl
       real vr(lcr,nmax)
